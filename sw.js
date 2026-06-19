c01c6ab9d298195b8a7e3c3238ec2e1c99d4818a
efim26470-ops <efim26470@gmail.com>
2026-06-19 20:43:54 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-// CS2 Case Lab v30: service worker disabled intentionally to avoid stale GitHub Pages cache.
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(self.registration.unregister()));
